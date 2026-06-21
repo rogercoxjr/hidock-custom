360653a9d920bf925bc6d828b7abb199577c99d7
Roger Cox <rcox@ii-us.com>
2026-06-20 21:54:35 -0500

    feat(library): re-export deriveCapturePeople + deriveCaptureLabels from utils

--- apps/electron/src/features/library/utils/index.ts
+++ apps/electron/src/features/library/utils/index.ts
@@ -4,4 +4,6 @@ export * from './formatTimestamp'
 export * from './getDisplayTitle'
 export * from './highlightText'
 export { buildSearchCorpus, buildDateAliases } from './buildSearchCorpus'
+export * from './deriveCapturePeople'
+export * from './deriveCaptureLabels'
 
